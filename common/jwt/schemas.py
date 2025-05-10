86294a5d407b4ca5c084c3765a1f58dd8aa61e99
exizman <yuraankin@yandex.ru>
2025-05-10 21:38:28 +0300

    init commit

---
+++ common/jwt/schemas.py
@@ -0,0 +1,92 @@
+
+from datetime import datetime
+from pydantic import BaseModel, PositiveInt, field_validator, EmailStr, UUID4, Field
+
+
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    phone: str | None = None
+    firstname: str | None = None
+    middlename: str | None = None
+    lastname: str | None = None
+    password: str | None = None
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    user_uuid: UUID4
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+
+class UserRegister(UserBase):
+    password: str
+    # confirm_password: str = Field(alias="repeatPassword")
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class JwtTokenSchema(BaseModel):
+    token: str
+    payload: dict
+    expire: datetime
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+
+class TokenPair(BaseModel):
+    access: JwtTokenSchema
+    refresh: JwtTokenSchema
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+
+
+class RefreshToken(BaseModel):
+    refresh: str
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+class AccessToken(BaseModel):
+    access: str
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+class SuccessResponseScheme(BaseModel):
+    msg: str
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class BlackListToken(BaseModel):
+    id: UUID4
+    expire: datetime
+
---
+++ docker_iternal/auth.Dockerfile
@@ -0,0 +1,40 @@
+FROM python:3.11-slim
+LABEL authors="exizman"
+
+WORKDIR /app
+
+
+
+COPY ./auth/requirements.txt ./requirements.txt
+
+RUN pip install -r requirements.txt
+#TODO fix this
+RUN pip install passlib bcrypt
+
+ENV PYTHONUNBUFFERED=1
+
+
+ARG POSTGRES_USER
+ARG POSTGRES_PASSWORD
+ARG POSTGRES_PORT
+ARG POSTGRES_NAME
+
+
+
+
+ENV POSTGRES_USER=$POSTGRES_USER
+ENV POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+ENV POSTGRES_HOST=$POSTGRES_HOST
+ENV POSTGRES_PORT=$POSTGRES_PORT
+ENV POSTGRES_NAME=$POSTGRES_NAME
+
+
+
+
+COPY ./auth /app/auth
+COPY ./db /app/db
+
+COPY ./common /app/common
+
+
+ENTRYPOINT ["uvicorn", "auth.run_web:app", "--host", "0.0.0.0", "--port", "8001"]
