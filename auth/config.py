dce3e4847816375cf63eb72bd2374accef1d70d6
exizman <yuraankin@yandex.ru>
2025-07-02 11:13:03 +0300

    достаточный билд

--- auth/config.py
+++ auth/config.py
@@ -17,3 +17,5 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_NAME}"
 
 settings = Settings()
+
+print("settings.CORS_ORIGINS:", settings.CORS_ORIGINS)
